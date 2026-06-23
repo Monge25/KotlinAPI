73cf5f39afd20be434bf35446202ace0aaf85c76
Monge25 <emilianomongeosuna@gmail.com>
2026-06-22 18:21:26 -0700

    Added functionality to use JWT

--- APIClientes/Controllers/AuthController.cs
+++ APIClientes/Controllers/AuthController.cs
@@ -1,6 +1,11 @@
 ﻿using APIClientes.Data;
+using APIClientes.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace APIClientes.Controllers
 {
@@ -9,7 +14,13 @@ namespace APIClientes.Controllers
     public class AuthController : ControllerBase
     {
         private readonly AppDbContext _db;
-        public AuthController(AppDbContext db) => _db = db;
+        private readonly IConfiguration _config;
+
+        public AuthController(AppDbContext db, IConfiguration config)
+        {
+            _db = db;
+            _config = config;
+        }
 
         public record LoginRequest(string Username, string Password);
 
@@ -18,7 +29,43 @@ namespace APIClientes.Controllers
         {
             var user = await _db.Usuarios
                 .FirstOrDefaultAsync(u => u.Username == req.Username && u.Password == req.Password);
-            return user is null ? Unauthorized("Credenciales inválidas") : Ok(new { mensaje = "OK", userId = user.Id });
+
+            if (user is null)
+                return Unauthorized("Credenciales inválidas");
+
+            var token = GenerarToken(user);
+
+            return Ok(new
+            {
+                mensaje = "OK",
+                userId = user.Id,
+                username = user.Username,
+                token
+            });
+        }
+
+        private string GenerarToken(Usuario user)
+        {
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.Username)
+        };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(8),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- APIClientes/Program.cs
+++ APIClientes/Program.cs
@@ -1,27 +1,48 @@
 using APIClientes.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(opt =>
     opt.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
-// Aplicar migraciones pendientes automáticamente (dev y producción)
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -30,6 +51,7 @@ using (var scope = app.Services.CreateScope())
 
 app.UseCors("AllowAll");
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
